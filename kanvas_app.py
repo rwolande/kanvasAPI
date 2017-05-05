baaf7ecb184aa6885412a73d46cbc23a3f264ed6
rwolande <wolande@branchapp.com>
2017-05-05 03:27:04 -0400

    update-kanvas app updated for true api

--- kanvas_app.py
+++ kanvas_app.py
@@ -1,9 +1,44 @@
-from flask import Flask
-app = Flask(__name__)
+from functools import wraps
+import datetime
+import logging
 
-@app.route('/')
-def hello_world():
-  return 'Hello from Flask!'
+from flask import Flask, current_app, request, jsonify, g
+from flask_restful import Resource, Api, reqparse, HTTPException
+from flask_mysqldb import MySQL
+
+import jwt
+import bcrypt
+
+from api.controllers.user import UserController
+#from utilities import get_log_level
+
+app = Flask(__name__, instance_relative_config=True)
+
+# Loads the configration from /instance/config.py
+# NOTE: This file is not checked into git - it must 
+# be defined for every installation
+#app.config.from_pyfile('config.py')
+
+app.api = Api(app)
+app.mysql = MySQL(app)
+
+# define routes
+app.api.add_resource(UserController, '/user/<int:user_id>')
+
+@app.before_request
+def before_request():
+
+	# Add anything you want to parse from the POST body
+	# to this array and it will be available in flask.g
+	post_parameters = ['name',
+					   'password',
+					   'image_source']
+					   
+	if not request.method == 'GET':
+		for param in post_parameters:
+			value = request.json[param] if param in request.json else None
+			setattr(g,param,value)
 
 if __name__ == '__main__':
   app.run()
+
